e4eda421073460ae03e761790e31bdd8d3210d3f
LP <luizpaulobborges@gmail.com>
2025-08-16 20:00:28 -0300

    style: remove margin right from dustpill

--- src/lib/components/DustPill.svelte
+++ src/lib/components/DustPill.svelte
@@ -1,3 +1,3 @@
-<span class="mr-2 rounded-full bg-orange-200 px-2.5 py-0.5 text-xs font-semibold text-orange-600"
-	>Dust</span
->
+<span class="rounded-full bg-orange-200 px-2.5 py-0.5 text-xs font-semibold text-orange-600">
+	Dust
+</span>
